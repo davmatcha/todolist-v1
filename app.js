ee8ac559db54a305923c9e7f8c9dcac13e3237be
David <david.chalmers117@gmail.com>
2020-08-18 04:43:29 +0100

    Add style and interactivity to todo list and work, about pages.

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = ["Buy food", "Cook food", "Eat food"];
+let workItems = [];
+
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
-
-let items = ["Buy food", "Cook food", "Eat food"];
+app.use(express.static("public"));
 
 app.get("/", function(req, res) { 
     let today = new Date();
@@ -18,14 +20,36 @@ app.get("/", function(req, res) {
     };
 
     let day = today.toLocaleDateString("en-US", options);
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
     });
+
+
 
 app.post("/", function(req, res){
     let item = req.body.newItem;
-    console.log(item);
-    items.push(item);
-    res.redirect("/");
+    if (req.body.list === "Work") {
+        console.log(item);
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        console.log(item);
+        items.push(item);
+        res.redirect("/");
+    }  
+});
+
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
+
+app.post("/work", function(req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
+
+app.get("/about", function(req, res) {
+    res.render("about");
 });
 
 app.listen(3000, function () {
