7776121159a125145c7172fd49b7ceb3d30bfab7
David <david.chalmers117@gmail.com>
2020-08-18 23:11:29 +0100

    Finish refining code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const date = require(__dirname + "/date.js");
 const app = express();
 
 let items = ["Buy food", "Cook food", "Eat food"];
@@ -12,14 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) { 
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
+    let day = date.getDate();
     res.render("list", {listTitle: day, newListItems: items});
     });
 
