e1fc8305495b5d26acd936a184151d6ebef5424b
David <david.chalmers117@gmail.com>
2020-08-18 02:23:03 +0100

    Finsh foundations of todo list app, can add a item to existing list with the date as title

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+let items = ["Buy food", "Cook food", "Eat food"];
+
+app.get("/", function(req, res) { 
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+    res.render("list", {kindOfDay: day, newListItems: items});
+    });
+
+app.post("/", function(req, res){
+    let item = req.body.newItem;
+    console.log(item);
+    items.push(item);
+    res.redirect("/");
+});
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
